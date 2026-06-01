36cd05408e59d2baaf2dba993c6e3723b67c106e
Gonçalo <goncaloalmeida.artwork@gmail.com>
2026-06-01 11:41:26 +0100

    Adicionado o timmer

--- GD2_TP2/Assets/Scripts/Temporizador/tempofinal.cs
+++ GD2_TP2/Assets/Scripts/Temporizador/tempofinal.cs
@@ -1,13 +1,17 @@
 using System;
 using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class tempofinal : MonoBehaviour
 {
 
-    [SerializeField] TextMeshPro timerText;
+    [SerializeField] TextMeshProUGUI timerText;
+    [SerializeField]
+    private string _sceneName;
 
-    [SerializeField] float tempo = 100f;
+    [SerializeField] float tempo = 30f;
 
     void Update()
     {
@@ -18,9 +22,15 @@ public class tempofinal : MonoBehaviour
             if (tempo < 0)
             {
                 tempo = 0;
+                MataGato();
             }
                 
             timerText.text = Convert.ToString(tempo);
         }
+    }
+
+    void MataGato()
+    {
+        SceneManager.LoadScene(_sceneName);
     }
 }
